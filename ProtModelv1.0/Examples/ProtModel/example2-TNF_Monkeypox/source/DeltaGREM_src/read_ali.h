404aebc0112b58a1ae36e7dc5408c304c842150f
DavidFerreiro <98025308+DavidFerreiro@users.noreply.github.com>
2022-06-23 17:14:05 +0200

    Delete ProtModelv1.0/Examples/ProtModel/example2-TNF_Monkeypox directory

--- ProtModelv1.0/Examples/ProtModel/example2-TNF_Monkeypox/source/DeltaGREM_src/read_ali.h
+++
@@ -1,2 +0,0 @@
-int Read_ali(float **freq_ia, float *Seq_id, short ***ali_seq, int *N_ali,
-	     char *file_ali, short *i_seq, int L_seq_pdb, int L_str_pdb);
--- ProtModelv1.0/Examples/ProtModel/example2-TNF_Monkeypox/source/DeltaGREM_src/read_str_mut.h
+++
@@ -1,2 +0,0 @@
-float **Str_mut_matrix(char *STR_MUT_TYPE, char *file_str_mut,
-		       struct protein target, int *res_index);
