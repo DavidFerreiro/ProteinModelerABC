2bab763067943fe97c8ca487375caeb77cf79615
DavidFerreiro <98025308+DavidFerreiro@users.noreply.github.com>
2022-10-17 15:23:08 +0100

    Delete ProtModelv1.0/Examples/ProtModel_Cluster/example-4-C30-2 directory

--- ProtModelv1.0/Examples/ProtModel_Cluster/example-4-C30-2/SimulationsOutputs/launch_F_Results.py
+++
@@ -1,8 +0,0 @@
-import Functions
-import multiprocessing
-
-if __name__ == "__main__":
-  a_pool = multiprocessing.Pool(500)
-  result = a_pool.map(Functions.SummaryResultF, range(10000, 20000), chunksize=1)
-  a_pool.close()
-  a_pool.join()
