cd71d611726299f125d2a29014941202e94372a6
DavidFerreiro <98025308+DavidFerreiro@users.noreply.github.com>
2022-10-17 16:34:38 +0100

    Add files via upload

---
+++ ProtModelv1.0/ProtModel/source/DeltaGREM_src/allocate.h
@@ -0,0 +1,6 @@
+int ** Allocate_mat2_i(int n1, int n2, char *);
+float  **Allocate_mat2_f(int n1, int n2, char *);
+double **Allocate_mat2_d(int n1, int n2, char *);
+void Empty_matrix_i(int **matrix, int N);
+void Empty_matrix_f(float **matrix, int N);
+void Empty_matrix_d(double **matrix, int N);
