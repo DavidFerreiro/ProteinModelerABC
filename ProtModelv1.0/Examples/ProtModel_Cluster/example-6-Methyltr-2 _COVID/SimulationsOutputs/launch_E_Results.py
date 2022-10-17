dace131bfd0d42563a589cf5b5357bc57d47070f
DavidFerreiro <98025308+DavidFerreiro@users.noreply.github.com>
2022-10-17 15:23:53 +0100

    Delete ProtModelv1.0/Examples/ProtModel_Cluster/example-6-Methyltr-2 _COVID directory

--- ProtModelv1.0/Examples/ProtModel_Cluster/example-6-Methyltr-2 _COVID/SimulationsOutputs/launch_E_Results.py
+++
@@ -1,8 +0,0 @@
-import Functions
-import multiprocessing
-
-if __name__ == "__main__":
-  a_pool = multiprocessing.Pool(500)
-  result = a_pool.map(Functions.SummaryResultE, range(0, 10000), chunksize=1)
-  a_pool.close()
-  a_pool.join()
