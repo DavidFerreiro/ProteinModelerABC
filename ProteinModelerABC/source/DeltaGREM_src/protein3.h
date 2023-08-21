0eae4018b7e0b3853b8eaee28bc5c1a33084d465
DavidFerreiro <98025308+DavidFerreiro@users.noreply.github.com>
2023-08-21 09:40:12 +0200

    Add files via upload

---
+++ ProteinModelerABC/source/DeltaGREM_src/protein3.h
@@ -0,0 +1,70 @@
+#define IJ_MIN 3  // Minimum value of |i-j| for contacts 
+#define N_PROT_MAX 10000
+#define N_PARAM 211
+#define N_STR 100
+
+/******************************
+
+ *          Functions         *
+ 
+ ******************************/
+
+extern int Threading(short *, float *, float *, float, float *, int);
+extern int Ini_thread(char *, int);
+
+/*****************************
+
+ *         Variables         *
+ 
+ *****************************/
+
+struct contact{
+  short res1;
+  short res2;
+};
+
+struct protein{
+  char name[120];
+  // based on conformation
+  int L_PDB;  // number of residues with PDB coord
+  int nchain;
+  int *ini_chain;
+  int *len_chain;
+  short *aa_seq;
+  char *sec_str;
+  int *i_sec;
+  // based on seqres
+  int length; // length of unique sequence
+  char *seqres;
+  int nc_seqres;
+  int *ini_seqres;
+  int *len_seqres;
+  // Correspondence
+  int *res_index;
+  // contacts
+  int n_cont;
+  short **contact;
+  struct contact *cont_list;
+};
+
+struct state{
+  short first;
+  float energy;
+  float overlap;
+  float alpha;
+  short n_cont;
+  short **cont;
+  struct protein *prot_ptr;
+};
+
+extern struct protein prot[N_PROT_MAX], target;
+//extern float **interactions;
+
+
+
+
+
+
+
+
+
